dec409d292a7aac4bbc4be22aa91633fb9ac850f
itzfahadoooo <foyindolapo@gmail.com>
2025-03-30 23:34:48 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
