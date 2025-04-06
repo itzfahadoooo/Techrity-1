f9ae6cb21654b674a1764318a2edbb480ed1fffa
itzfahadoooo <foyindolapo@gmail.com>
2025-04-06 21:36:02 +0100

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
 
   return (
 
-    
+
     <div className="grid md:grid-cols-3">
       <div className="md:col-span-1 md:flex md:justify-end">
         <nav className="text-right">
@@ -58,6 +58,7 @@ const App = () => {
                   strokeLinejoin="round"
                   strokeWidth="2"
                   stroke="currentColor"
+                  
                   viewBox="0 0 24 24"
                 >
                   <path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
